af246c61e3b506903b748f547a9093d6b6dd43fa
Se7eNFTW <116437726+Se7eNFTW@users.noreply.github.com>
2026-04-03 12:58:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# To run and test the code you need to update 4 places:
+#To run and test the code you need to update 4 places:
 # 1. Change MY_EMAIL/MY_PASSWORD to your own details.
 # 2. Go to your email provider and make it allow less secure apps.
 # 3. Update the SMTP ADDRESS to match your email provider.
@@ -12,23 +12,22 @@ import random
 import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = "soumilbiswas87@gmail.com"
+MY_PASSWORD = "vsitiuuiomjphrnc"
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
@@ -36,3 +35,4 @@ if today_tuple in birthdays_dict:
             to_addrs=birthday_person["email"],
             msg=f"Subject:Happy Birthday!\n\n{contents}"
         )
+
